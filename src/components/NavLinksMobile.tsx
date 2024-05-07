aa7a17172b7f0356083d12406cd75fc8c378c4b0
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2024-05-07 09:53:52 +0300

    UPDATE: Active class state for mobile sidebar.

--- src/components/NavLinksMobile.tsx
+++ src/components/NavLinksMobile.tsx
@@ -1,8 +1,24 @@
+"use client";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
 import { SheetContent, SheetClose } from "@/components/ui/sheet";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import Link from "next/link";
 
 const NavLinksMobile = () => {
+  const pathname = usePathname();
+  const isActive = (path: string) => {
+    // Check if the path is the current route
+    return pathname === path;
+  };
+
+  const getLinkClass = (path: string) => {
+    return clsx("transition-colors hover:text-foreground", {
+      "text-foreground": isActive(path),
+      "text-muted-foreground": !isActive(path),
+    });
+  };
+
   return (
     <SheetContent side="left">
       <nav className="grid gap-6 text-lg font-medium">
@@ -18,28 +34,19 @@ const NavLinksMobile = () => {
             </span>
           </div>
         </div>
-        <Link href="/" className="hover:text-foreground">
+        <Link href="/" className={getLinkClass("/")}>
           <SheetClose> Home</SheetClose>
         </Link>
-        <Link
-          href="/about"
-          className="text-muted-foreground hover:text-foreground"
-        >
+        <Link href="/about" className={getLinkClass("/about")}>
           <SheetClose>About</SheetClose>
         </Link>
-        <Link
-          href="/works"
-          className="text-muted-foreground hover:text-foreground"
-        >
+        <Link href="/works" className={getLinkClass("/works")}>
           <SheetClose>Works</SheetClose>
         </Link>
-        <Link
-          href="/blog"
-          className="text-muted-foreground hover:text-foreground"
-        >
+        <Link href="/blog" className={getLinkClass("/blog")}>
           <SheetClose>Blog</SheetClose>
         </Link>
-        <Link href="#" className="text-muted-foreground hover:text-foreground">
+        <Link href="#" className={getLinkClass("/cv")}>
           CV
         </Link>
       </nav>
