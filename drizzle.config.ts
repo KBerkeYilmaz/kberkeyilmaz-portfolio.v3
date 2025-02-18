7b8cac488fdfb919f68f979922b7ddd23575a499
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2025-02-18 23:20:55 +0300

    wip: fixed drizzle config.

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,13 @@
-import { defineConfig } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
 import { env } from "@/env";
 
-export default defineConfig({
+export default ({
   schema: "./src/server/db/schema.ts",
   out: "./src/drizzle/migrations",
-  dialect: "postgresql",
+  driver: "pg",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    connectionString: env.DATABASE_URL,
   },
   verbose: true,
   tablesFilter: ["kberkeyilmaz-portfolio.v3_*"],
-}) 
+}) satisfies Config;
